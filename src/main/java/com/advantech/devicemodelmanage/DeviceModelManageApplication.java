7fc832fc4fa2f93332e5dc1277f2287a0a2339e1
tomlover <543121424@qq.com>
2018-08-06 16:53:04 +0800

    init project

---
+++ src/main/java/com/advantech/devicemodelmanage/DeviceModelManageApplication.java
@@ -0,0 +1,12 @@
+package com.advantech.devicemodelmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceModelManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceModelManageApplication.class, args);
+    }
+}
